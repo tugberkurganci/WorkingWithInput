572813f8465fa8955f47876bdc4b2f5a20f5c111
tugberkurganci <121198088+tugberkurganci@users.noreply.github.com>
2023-01-03 15:20:34 +0300

    round

---
+++ src/RoundImplementation.java
@@ -0,0 +1,14 @@
+public class RoundImplementation {
+
+    public double aDoubleToRound(double input) {
+
+
+        return Math.round(input);
+    }
+
+    public double aFloatToRound(float input) {
+
+
+        return Math.round(input);
+    }
+}
